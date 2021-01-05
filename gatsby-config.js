b585d429a85f75e6588e007e4d33a43a0c8b15f8
Andrew Carrigan <carrigan223@gmail.com>
2021-01-05 14:18:35 -0800

    trying to get build to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "McCarron Auto",
     description: "The Berskhires most dependable stop for auto care",
+    siteUrl: "https://needstobetheproductionurl.com",
   },
   plugins: [
     "gatsby-plugin-emotion",
